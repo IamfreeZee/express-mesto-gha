16b8c060c0667b4986e0e09317523ba9732addb1
freeZeeee <freeze_o@mail.ru>
2023-09-27 16:40:02 +0300

    fix: тесты юзера

--- controllers/users.js
+++ controllers/users.js
@@ -10,25 +10,29 @@ const addUser = (req, res) => {
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch(() => res.status(400).send({ message: 'На сервере произошла ошибка' }));
+    .catch(() => res.status(500).send({ message: 'На сервере произошла ошибка' }));
 };
 
 const getUserById = (req, res) => {
-  User.findById(req.params.userId)
-    .then((user) => res.send(user))
-    .catch(() => res.status(404).send({ message: 'На сервере произошла ошибка' }));
+  if (req.params.userId.length === 24) {
+    User.findById(req.params.userId)
+      .then((user) => res.send(user))
+      .catch(() => res.status(404).send({ message: 'Пользователь с таким Id не найден' }));
+  } else {
+    res.status(400).send({ message: 'Некоректный Id пользователя' });
+  }
 };
 
 const editUserData = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => res.send(user))
     .catch(() => res.status(400).send({ message: 'На сервере произошла ошибка' }));
 };
 
 const editUserAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => res.send(user))
     .catch(() => res.status(400).send({ message: 'На сервере произошла ошибка' }));
 };
